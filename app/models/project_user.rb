074e865309d55d2e94e4809f1779c976f247a8c8
Charles Engen <owenengen@gmail.com>
2021-05-02 04:35:29 +0000

    Got devise and the basic project framework workin correctly. Need to fix devise to work as intended.

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,4 +1,5 @@
-class ProjectUser < ApplicationRecord
+class ProjectUser < ApplicationRecord 
+  validates :data, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
   devise_for :project_users
+  root 'projects#index'
+  get '/projects', to: "projects#index"
+  get "/projects/:id", to: "projects#show"
+  get 'project_users/sign_out', to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
