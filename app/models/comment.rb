c086362eac45f6a41c6fc73cd473e0b6da312e54
Charles Engen <owenengen@gmail.com>
2021-05-02 03:07:13 +0000

    Added state to comments and has_many to comments in project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :project
+
+  enum state: [:not_reviewed, :published, :hidden]
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  has_many :comments
 end
