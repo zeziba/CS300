150049c353b246bdb9f9c020a22cd301ad42bb44
“Charles <owenengen@gmail.com>
2021-05-22 05:22:38 +0000

    Added several needed values to projects database -> {name, img, uri, last_viewed, clicks}

---
+++ db/migrate/20210522051931_add_data_to_projects.rb
@@ -0,0 +1,9 @@
+class AddDataToProjects < ActiveRecord::Migration[5.2]
+  def change
+    add_column :projects, :clicks, :integer
+    add_column :projects, :last_viewed, :datetime
+    add_column :projects, :uri, :string
+    add_column :projects, :img, :binary
+    add_column :projects, :name, :string
+  end
+end
